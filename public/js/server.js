b43a97b6eb7a98df6e303c0e35bd7493b5683aae
Kunal Pitale <kunal.212034105@vcet.edu.in>
2024-09-19 22:05:06 +0530

    refactored

--- public/js/server.js
+++ public/js/server.js
@@ -1,11 +1,3 @@
-console.log("GOAT");
-
-// fetch("https://puzzle.mead.io/puzzle").then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
 const weatherForm = document.querySelector("form");
 const searchInput = document.querySelector("input");
 const msg1 = document.querySelector("#message-1");
